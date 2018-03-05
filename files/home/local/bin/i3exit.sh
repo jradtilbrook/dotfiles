b0900e093dc7cb20821099b5f2804f919d83713f
jradtilbrook <jrad.tilbrook@gmail.com>
2018-03-05 18:07:33 +0800

    Replace browserpass with rofi/dmenu script

--- files/home/local/bin/i3exit.sh
+++ files/home/local/bin/i3exit.sh
@@ -7,7 +7,7 @@ options="Cancel\nLock\nSleep\nLog out\nShutdown\nRestart"
 # colours
 colour="#a0000000,#a3a4a1,#a0000000,#da000000,#80fa334a"
 # prompt user using rofi
-select=$(echo -e $options | rofi -dmenu -p "What would you like to do:" -color-normal $colour)
+select=$(echo -e $options | rofi -dmenu -p "exit" -color-normal $colour)
 # do the appropriate action
 case $select in
     "Lock") # Lock

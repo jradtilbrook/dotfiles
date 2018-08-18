731bd40fed62d4ed1de284c9a16855996bee6821
jradtilbrook <jrad.tilbrook@gmail.com>
2018-08-18 12:26:56 +0800

    Improve i3 lock script to fix image resolution

--- files/home/local/bin/i3lock_wrapper.sh
+++ files/home/local/bin/i3lock_wrapper.sh
@@ -11,15 +11,25 @@
 mpc -h $HOME/.config/mpd/socket pause
 
 # Ensure dmenu shading is removed before screenshot
-sleep 0.2
+# sleep 0.2
 
 # pause dunst
 killall -SIGUSR1 dunst
 
+# create a temp file for resizing images
+RESIZED_IMAGE_PATH=$(mktemp)
+IMAGE_PATH=~/.wallpaper
+# determine the current screen resolution. NB: this probably only works for a single screen
+RESOLUTION=$(xrandr -q | head -1 | sed -n 's:.*current\s\([0-9]\+\)\sx\s\([0-9]\+\).*:\1x\2:p')
+# LOCK_IMAGE="--blur 6"
+# TODO: see if the processing time can be reduced - this conversion takes a few seconds
+convert $IMAGE_PATH -resize $RESOLUTION^ -gravity center -extent $RESOLUTION $RESIZED_IMAGE_PATH
+LOCK_IMAGE="-i $RESIZED_IMAGE_PATH -t"
+
 # lock the screen
 i3lock \
 --ignore-empty-password \
--i ~/.wallpaper -t \ #--blur 3 \
+$LOCK_IMAGE \
 --radius=100 \
 --indicator \
 --clock \
@@ -42,5 +52,8 @@ $*
 
 # resume dunst
 killall -SIGUSR2 dunst
+
+# cleanup temp files
+rm -f $RESIZED_IMAGE_PATH
 
 exit 0

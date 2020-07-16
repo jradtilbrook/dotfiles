a0269e8a4decdbfc3aa080ed5f1a01231c5cf6b2
jradtilbrook <jrad.tilbrook@gmail.com>
2020-07-16 13:17:48 +0800

    Improve bluetooth script and i3 blocklet

--- files/home/local/bin/bluetooth.sh
+++ files/home/local/bin/bluetooth.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 # This script can be run to prompt for bluetooth devices to connect to
 
-# show this prompt with a blue hint
-theme="element selected.normal { text-color: #33B5E1; }"
-select=$(bluetoothctl devices | sed 's/Device \(\w\)/\1/g' | rofi -dmenu -p "device" -theme-str "$theme" -no-custom | sed 's/\([^ ]\+\).*/\1/g')
+# show this prompt with colour
+if [ $1 = "connect" ]; then
+    # blue if connecting
+    TEXT_COLOUR="#33B5E1"
+else
+    # otherwise red
+    TEXT_COLOUR="#EF5253"
+fi
+
+theme="element selected.normal { text-color: $TEXT_COLOUR; }"
+select=$(bluetoothctl devices | sed 's/Device \(\w\)/\1/g' | rofi -dmenu -p "$1" -theme-str "$theme" -no-custom | sed 's/\([^ ]\+\).*/\1/g')
 [ -z "$select" ] && exit 0
 
-# connect to the provided device
-bluetoothctl connect "$select"
+case $1 in
+    connect)
+        bluetoothctl connect "$select"
+    ;;
+    disconnect)
+        bluetoothctl disconnect "$select"
+    ;;
+esac

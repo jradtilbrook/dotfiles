e5c6331b38c535eefc31186e2467708dae46d3c2
jradtilbrook <jradtilbrook@hey.com>
2020-11-20 15:46:48 +0800

    Remove lock from exit script since its never used

--- files/home/local/bin/i3exit.sh
+++ files/home/local/bin/i3exit.sh
@@ -3,16 +3,13 @@
 # Prompts the user for confirmation and sets up colours to suite current theme.
 
 # menu options
-options="Cancel\nLock\nSleep\nLog out\nShutdown\nRestart"
+options="Cancel\nSleep\nLog out\nShutdown\nRestart"
 # override the colours
-theme="listview { lines: 6; } element selected.normal { text-color: #EF5253; }"
+theme="listview { lines: 5; } element selected.normal { text-color: #EF5253; }"
 # prompt user using rofi
 select=$(echo -e $options | rofi -dmenu -p "exit" -theme-str "$theme")
 # do the appropriate action
 case $select in
-    "Lock")
-        ~/.local/bin/i3lock_wrapper.sh
-        ;;
     "Sleep")
         systemctl suspend-then-hibernate
         ;;

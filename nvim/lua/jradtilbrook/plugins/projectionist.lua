e7af989e0c2103c2f24d89435c202178fc0afb8a
jradtilbrook <jarryd@tilbrooktech.com>
2023-12-14 15:04:09 +0800

    Add go projects to projectionist

--- nvim/lua/jradtilbrook/plugins/projectionist.lua
+++ nvim/lua/jradtilbrook/plugins/projectionist.lua
@@ -1,44 +1,59 @@
 return {
-  'tpope/vim-projectionist',
-  dependencies = {
-    'tpope/vim-dispatch',
-  },
-  config = function()
-    vim.g.projectionist_heuristics = {
-      artisan = {
-        ['*'] = {
-          start = 'sail up',
-          console = 'sail tinker',
-          make = 'npm run dev',
-        },
-        ['app/Models/*.php'] = {
-          type = 'model',
-        },
-        ['app/Http/Controllers/*.php'] = {
-          type = 'controller',
-        },
-        ['routes/*.php'] = {
-          type = 'route',
-        },
-        ['database/migrations/*.php'] = {
-          type = 'migration',
-        },
-        ['app/*.php'] = {
-          type = 'source',
-          alternate = {
-            'tests/Feature/{}Test.php',
-            'tests/Unit/{}Test.php',
-          },
-        },
-        ['tests/Feature/*Test.php'] = {
-          type = 'test',
-          alternate = 'app/{}.php',
-        },
-        ['tests/Unit/*Test.php'] = {
-          type = 'test',
-          alternate = 'app/{}.php',
-        },
-      },
-    }
-  end
+    "tpope/vim-projectionist",
+    lazy = false,
+    dependencies = {
+        "tpope/vim-dispatch",
+    },
+    config = function()
+        vim.g.projectionist_heuristics = {
+            artisan = {
+                ["*"] = {
+                    start = "sail up",
+                    console = "sail tinker",
+                    make = "npm run dev",
+                },
+                ["app/Models/*.php"] = {
+                    type = "model",
+                },
+                ["app/Http/Controllers/*.php"] = {
+                    type = "controller",
+                },
+                ["routes/*.php"] = {
+                    type = "route",
+                },
+                ["database/migrations/*.php"] = {
+                    type = "migration",
+                },
+                ["app/*.php"] = {
+                    type = "source",
+                    alternate = {
+                        "tests/Feature/{}Test.php",
+                        "tests/Unit/{}Test.php",
+                    },
+                },
+                ["tests/Feature/*Test.php"] = {
+                    type = "test",
+                    alternate = "app/{}.php",
+                },
+                ["tests/Unit/*Test.php"] = {
+                    type = "test",
+                    alternate = "app/{}.php",
+                },
+            },
+            ["go.mod"] = {
+                ["*.go"] = {
+                    type = "source",
+                    alternate = {
+                        "{}_test.go",
+                    },
+                },
+                ["*_test.go"] = {
+                    type = "test",
+                    alternate = {
+                        "{}.go",
+                    },
+                },
+            },
+        }
+    end,
 }

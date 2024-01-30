30ee03717045245dda41181c1babf87d75c3e9cd
jradtilbrook <jarryd@tilbrooktech.com>
2024-01-30 09:57:41 +0800

    Refactor hammerspoon
    Inspired by https://github.com/jesseleite/dotfiles/blob/master/hammerspoon/init.lua

---
+++ hammerspoon/apps.lua
@@ -0,0 +1,34 @@
+return {
+    Kitty = {
+        id = "net.kovidgoyal.kitty",
+        summonModal = "t", -- terminal
+    },
+    Linear = {
+        id = "com.linear",
+        summonModal = "l",
+    },
+    Messages = {
+        id = "com.apple.MobileSMS",
+        summonModal = "i", -- imessage
+    },
+    Mimestream = {
+        id = "com.mimestream.Mimestream",
+        summonModal = "e", -- email
+    },
+    Notion = {
+        id = "notion.id",
+        summonModal = "n",
+    },
+    Slack = {
+        id = "com.tinyspeck.slackmacgap",
+        summonModal = "s",
+    },
+    Spotify = {
+        id = "com.spotify.client",
+        summonModal = "m", -- music
+    },
+    Zoom = {
+        id = "us.zoom.xos",
+        summonModal = "z",
+    },
+}
--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,82 +1,52 @@
-local modal = hs.hotkey.modal.new({ "cmd", "ctrl", "alt", "shift" }, "return", nil)
+local hyper = { "cmd", "ctrl", "alt", "shift" }
 -- used as a nested modal
-local webModal = hs.hotkey.modal.new(nil, nil, nil)
+-- local webModal = hs.hotkey.modal.new(nil, nil, nil)
 
-local function bind(modal, name)
+require("helpers")
+apps = require("apps")
+local summon = require("summon")
+
+-- map table of apps to key binding -> bundle ID
+local summonModalBindings = tableFlip(hs.fnutils.map(apps, function(app)
+    return app.summonModal
+end))
+summonModalBindings = hs.fnutils.map(summonModalBindings, function(app)
     return function()
-        -- find a running app
-        local app = hs.application(name)
-        if app == nil then
-            -- launch it if not running
-            hs.application.open(name)
-        else
-            -- otherwise if its focused, hide it
-            if app:isFrontmost() then
-                app:hide()
-            else
-                -- or focus if not focused
-                app:activate()
-            end
-        end
-        modal:exit()
+        summon(app)
     end
-end
-
--- automatically cancel the modal after a timeout
-local timer = hs.timer.new(2, function()
-    modal:exit()
-end)
-local webTimer = hs.timer.new(2, function()
-    webModal:exit()
 end)
 
-function modal:entered()
-    timer:start()
-end
+summonModalBindings["r"] = hs.reload
+summonModalBindings["a"] = hs.spaces.toggleLaunchPad
+summonModalBindings["d"] = hs.spaces.toggleShowDesktop
 
-function webModal:entered()
-    webTimer:start()
-end
+registerModalBindings(hyper, "return", summonModalBindings, true)
 
-function modal:exited()
-    timer:stop()
-end
-
-function webModal:exited()
-    webTimer:stop()
-end
-
-modal:bind(nil, "escape", nil, function()
-    modal:exit()
-end)
-
-modal:bind(nil, "r", nil, hs.reload)
-
-modal:bind(nil, "a", nil, function()
-    hs.spaces.toggleLaunchPad()
-    modal:exit()
-end)
-modal:bind(nil, "c", nil, bind(modal, "messages"))
-modal:bind(nil, "d", nil, function()
-    hs.spaces.toggleShowDesktop()
-    modal:exit()
-end)
-modal:bind(nil, "e", nil, bind(modal, "mimestream"))
-modal:bind(nil, "l", nil, bind(modal, "linear"))
-modal:bind(nil, "m", nil, bind(modal, "spotify"))
-modal:bind(nil, "n", nil, bind(modal, "notion"))
-modal:bind(nil, "s", nil, bind(modal, "slack"))
-modal:bind(nil, "t", nil, bind(modal, "kitty"))
-modal:bind(nil, "z", nil, bind(modal, "zoom.us"))
-modal:bind(nil, "b", nil, function()
-    webModal:enter()
-    modal:exit()
-end)
-
--- a sub modal to focus a specific instance of google chrome
--- TODO: switch to the specific chrome profile for work/home
-webModal:bind(nil, "h", nil, bind(webModal, "google chrome"))
-webModal:bind(nil, "w", nil, bind(webModal, "google chrome"))
+-- modal:bind(nil, "b", nil, function()
+--     webModal:enter()
+--     modal:exit()
+-- end)
+--
+-- -- a sub modal to focus a specific instance of google chrome
+-- -- TODO: switch to the specific chrome profile for work/home
+-- webModal:bind(nil, "h", nil, function()
+--     local name = "google chrome"
+--     local app = hs.application(name)
+--     if app == nil then
+--         -- launch it if not running
+--         hs.application.open(name)
+--     else
+--         -- otherwise if its focused, hide it
+--         if app:isFrontmost() then
+--             app:hide()
+--         else
+--             -- or focus profile window
+--             -- TODO
+--             app:activate()
+--         end
+--     end
+--     webModal:exit()
+-- end)
 
 -- start a watcher to automatically reload on changes
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", hs.reload):start()

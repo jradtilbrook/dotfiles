c97383124f0fb20410a91236c1bf0a001f4d8be3
jradtilbrook <jarryd@tilbrooktech.com>
2024-04-15 09:02:04 +0800

    Revert "Add copilot cmp"
    This reverts commit 7f5ddba6cff1b5ae1912cb0dbf647f1834b49ab0.

--- nvim/lua/jradtilbrook/plugins/copilot.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "zbirenbaum/copilot.lua",
-    config = {
-        -- It is recommended to disable copilot.lua's suggestion and panel modules, as they can interfere with completions properly appearing in copilot-cmp
-        panel = { enabled = false },
-        suggestion = { enabled = false },
-    },
-}

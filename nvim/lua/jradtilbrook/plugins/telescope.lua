8264e16722f2884fc253d6a5e74309d7d39ef8d0
jradtilbrook <jarryd@tilbrooktech.com>
2024-05-06 10:24:09 +0800

    Improve telescope
    Allow opening multiple files and path name truncating

--- nvim/lua/jradtilbrook/plugins/telescope.lua
+++ nvim/lua/jradtilbrook/plugins/telescope.lua
@@ -6,6 +6,21 @@ return {
         config = function()
             local actions = require("telescope.actions")
 
+            local select_one_or_multi = function(prompt_bufnr)
+                local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+                local multi = picker:get_multi_selection()
+                if not vim.tbl_isempty(multi) then
+                    require("telescope.actions").close(prompt_bufnr)
+                    for _, j in pairs(multi) do
+                        if j.path ~= nil then
+                            vim.cmd(string.format("%s %s", "edit", j.path))
+                        end
+                    end
+                else
+                    require("telescope.actions").select_default(prompt_bufnr)
+                end
+            end
+
             require("telescope").setup({
                 pickers = {
                     buffers = {
@@ -56,6 +71,7 @@ return {
                     },
                 },
                 defaults = {
+                    path_display = { "truncate" },
                     layout_config = {
                         prompt_position = "top",
                     },
@@ -64,6 +80,7 @@ return {
                         i = {
                             ["<C-j>"] = "move_selection_next",
                             ["<C-k>"] = "move_selection_previous",
+                            ["<CR>"] = select_one_or_multi,
                         },
                     },
                 },

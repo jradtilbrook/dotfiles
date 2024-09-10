d09fe6f523fa1697488474c9240f85510583d013
jradtilbrook <jarryd@tilbrooktech.com>
2024-09-10 16:13:29 +0800

    Update gitsigns

--- nvim/lua/jradtilbrook/plugins/gitsigns.lua
+++ nvim/lua/jradtilbrook/plugins/gitsigns.lua
@@ -1,10 +1,36 @@
 return {
     "lewis6991/gitsigns.nvim",
-    lazy = false,
+    event = "VimEnter",
     opts = {
         current_line_blame_opts = {
             delay = 200,
         },
+        on_attach = function(bufnr)
+            local gitsigns = require("gitsigns")
+
+            local function map(mode, l, r, opts)
+                opts = opts or {}
+                opts.buffer = bufnr
+                vim.keymap.set(mode, l, r, opts)
+            end
+
+            -- Navigation
+            map("n", "]c", function()
+                if vim.wo.diff then
+                    vim.cmd.normal({ "]c", bang = true })
+                else
+                    gitsigns.nav_hunk("next")
+                end
+            end)
+
+            map("n", "[c", function()
+                if vim.wo.diff then
+                    vim.cmd.normal({ "[c", bang = true })
+                else
+                    gitsigns.nav_hunk("prev")
+                end
+            end)
+        end,
     },
     keys = {
         { "[h",         "<cmd>Gitsigns prev_hunk<cr>",                 desc = "Previous hunk" },
@@ -31,7 +57,4 @@ return {
             desc = "Add worktree",
         },
     },
-    config = function(plugin, opts)
-        require("gitsigns").setup(opts)
-    end,
 }

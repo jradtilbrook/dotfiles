8b3c871d6c92976084c02df1e873f9485b49b5ab
jradtilbrook <jarryd@tilbrooktech.com>
2024-10-05 17:41:11 +0800

    Update cmp

--- nvim/lua/jradtilbrook/plugins/cmp.lua
+++ nvim/lua/jradtilbrook/plugins/cmp.lua
@@ -1,74 +1,114 @@
 return {
-  {
-    -- Autocompletion
-    'hrsh7th/nvim-cmp',
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
     dependencies = {
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-nvim-lsp-signature-help',
-      'hrsh7th/cmp-nvim-lsp',
-      -- TODO: Set up snippets
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-      'onsails/lspkind-nvim',
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-nvim-lsp-signature-help",
+        "hrsh7th/cmp-nvim-lsp",
+        {
+            "L3MON4D3/LuaSnip",
+            build = (function()
+                -- Build Step is needed for regex support in snippets.
+                -- This step is not supported in many windows environments.
+                -- Remove the below condition to re-enable on windows.
+                if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+                    return
+                end
+                return "make install_jsregexp"
+            end)(),
+            dependencies = {
+                {
+                    "rafamadriz/friendly-snippets",
+                    config = function()
+                        require("luasnip.loaders.from_vscode").lazy_load()
+                    end,
+                },
+            },
+        },
+        "saadparwaiz1/cmp_luasnip",
+        "onsails/lspkind-nvim",
     },
     config = function()
-      -- nvim-cmp setup
-      local cmp = require 'cmp'
-      local luasnip = require 'luasnip'
-      local lspkind = require 'lspkind'
+        -- nvim-cmp setup
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
+        local lspkind = require("lspkind")
 
-      luasnip.config.setup {}
+        luasnip.config.setup({})
 
-      cmp.setup {
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        formatting = {
-          format = lspkind.cmp_format(),
-        },
-        experimental = {
-          ghost_text = true,
-        },
-        mapping = cmp.mapping.preset.insert {
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete {},
-          ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          },
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-        },
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'nvim_lsp_signature_help' },
-          { name = 'intelephense' },
-          { name = 'luasnip' },
-        }, {
-          -- { name = 'buffer' },
-          { name = 'path' },
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
+            completion = { completeopt = "menu,menuone,noinsert" },
+            formatting = {
+                format = lspkind.cmp_format(),
+            },
+            experimental = {
+                ghost_text = true,
+            },
+            mapping = cmp.mapping.preset.insert({
+                -- Select the [n]ext item
+                ["<C-n>"] = cmp.mapping.select_next_item(),
+                -- Select the [p]revious item
+                ["<C-p>"] = cmp.mapping.select_prev_item(),
+
+                -- Scroll the documentation window [b]ack / [f]orward
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+
+                -- Accept ([y]es) the completion.
+                --  This will auto-import if your LSP supports it.
+                --  This will expand snippets if the LSP sent a snippet.
+                ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+
+                -- If you prefer more traditional completion keymaps,
+                -- you can uncomment the following lines
+                --['<CR>'] = cmp.mapping.confirm { select = true },
+                --['<Tab>'] = cmp.mapping.select_next_item(),
+                --['<S-Tab>'] = cmp.mapping.select_prev_item(),
+
+                -- Manually trigger a completion from nvim-cmp.
+                --  Generally you don't need this, because nvim-cmp will display
+                --  completions whenever it has completion options available.
+                ["<C-Space>"] = cmp.mapping.complete({}),
+
+                -- Think of <c-l> as moving to the right of your snippet expansion.
+                --  So if you have a snippet that's like:
+                --  function $name($args)
+                --    $body
+                --  end
+                --
+                -- <c-l> will move you to the right of each of the expansion locations.
+                -- <c-h> is similar, except moving you backwards.
+                ["<C-l>"] = cmp.mapping(function()
+                    if luasnip.expand_or_locally_jumpable() then
+                        luasnip.expand_or_jump()
+                    end
+                end, { "i", "s" }),
+                ["<C-h>"] = cmp.mapping(function()
+                    if luasnip.locally_jumpable(-1) then
+                        luasnip.jump(-1)
+                    end
+                end, { "i", "s" }),
+            }),
+            sources = cmp.config.sources({
+                {
+                    name = "lazydev",
+                    -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
+                    group_index = 0,
+                },
+                { name = "nvim_lsp" },
+                { name = "nvim_lsp_signature_help" },
+                { name = "intelephense" },
+                { name = "luasnip" },
+            }, {
+                { name = "buffer" },
+                { name = "path" },
+            }),
         })
-      }
-    end
-  },
+    end,
 }

ff3ce83826a50fb521c5a0931bb343ed45bc271b
jradtilbrook <jarryd@tilbrooktech.com>
2024-10-05 16:51:52 +0800

    Move some plugins to module

--- nvim/lua/jradtilbrook/plugins/init.lua
+++ nvim/lua/jradtilbrook/plugins/init.lua
@@ -55,6 +55,64 @@ return {
             end,
         },
     },
+    -- highlight and navigate comments
+    {
+        "folke/todo-comments.nvim",
+        event = "VimEnter",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+            signs = false,
+        },
+        keys = {
+            {
+                "[t",
+                function()
+                    require("todo-comments").jump_prev()
+                end,
+                desc = "Previous todo comment",
+                mode = { "n" },
+                silent = true,
+            },
+            {
+                "]t",
+                function()
+                    require("todo-comments").jump_next()
+                end,
+                desc = "Next todo comment",
+                mode = { "n" },
+                silent = true,
+            },
+        },
+    },
+
+    -- Extra text objects for HTML attributes
+    {
+        "whatyouhide/vim-textobj-xmlattr",
+        dependencies = {
+            "kana/vim-textobj-user",
+        },
+    },
+
+    -- Automatically insert matching brackets
+    { "windwp/nvim-autopairs", event = "InsertEnter", config = true },
+
+    -- Handy keymapping to split/join lines (eg arrays)
+    {
+        "bennypowers/splitjoin.nvim",
+        event = "VimEnter",
+        keys = {
+            { "<leader>cj", function() end, mode = { "n", "v" }, desc = "Join object under cursor" },
+            { "<leader>c,", function() end, mode = { "n", "v" }, desc = "Split object under cursor" },
+        },
+    },
+
+    -- Automatically fix indentation when pasting
+    {
+        "sickill/vim-pasta",
+        init = function()
+            vim.g.pasta_disabled_filetypes = { "fugitive" }
+        end,
+    },
 
     "tpope/vim-eunuch",
     "tpope/vim-abolish",
@@ -64,8 +122,8 @@ return {
         "tpope/vim-rhubarb",
         lazy = false,
         keys = {
-            { "<leader>gy", ":GBrowse!<cr>", desc = "Copy git URL",    mode = { "n", "v" }, silent = true },
-            { "<leader>gv", ":GBrowse<cr>",  desc = "View in browser", mode = { "n", "v" }, silent = true },
+            { "<leader>gy", ":GBrowse!<cr>", desc = "Copy git URL", mode = { "n", "v" }, silent = true },
+            { "<leader>gv", ":GBrowse<cr>", desc = "View in browser", mode = { "n", "v" }, silent = true },
         },
     },
     "tpope/vim-surround",

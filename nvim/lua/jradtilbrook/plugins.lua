00dcd8f5be84bec8e734b09eaa604a056fd77155
jradtilbrook <jarryd@tilbrooktech.com>
2024-10-05 16:45:03 +0800

    Remove worktree plugin

--- nvim/lua/jradtilbrook/plugins.lua
+++ nvim/lua/jradtilbrook/plugins.lua
@@ -28,7 +28,6 @@ return {
         },
     },
 
-
     -- Extra text objects for HTML attributes
     {
         "whatyouhide/vim-textobj-xmlattr",
@@ -55,29 +54,6 @@ return {
         "sickill/vim-pasta",
         init = function()
             vim.g.pasta_disabled_filetypes = { "fugitive" }
-        end,
-    },
-
-
-    {
-        "ThePrimeagen/git-worktree.nvim",
-        opts = {
-            update_on_change_command = "Telescope git_files",
-        },
-        config = function(plugin, opts)
-            local worktree = require("git-worktree")
-            worktree.setup(opts)
-            worktree.on_tree_change(function(op, metadata)
-                if op == worktree.Operations.Switch then
-                    -- update all other buffers to the worktree directory
-                    local windows = vim.api.nvim_list_wins()
-                    local buffers = vim.tbl_filter(function(bufn)
-                        return vim.api.nvim_buf_is_valid(bufn) and vim.api.nvim_buf_get_option(bufn, "buflisted")
-                    end, vim.api.nvim_list_bufs())
-                end
-            end)
-
-            require("telescope").load_extension("git_worktree")
         end,
     },
 }

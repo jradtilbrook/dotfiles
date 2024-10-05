ce4d4b5863848981b95742d5cc60aa4807c6f1b0
jradtilbrook <jarryd@tilbrooktech.com>
2024-10-05 17:40:15 +0800

    Switch neodev for lazydev

--- nvim/lua/jradtilbrook/plugins/lsp.lua
+++ nvim/lua/jradtilbrook/plugins/lsp.lua
@@ -52,7 +52,22 @@ return {
         { "j-hui/fidget.nvim", opts = {} },
 
         -- Additional lua configuration, makes nvim stuff amazing!
-        { "folke/neodev.nvim", opts = {} },
+        {
+            -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+            -- used for completion, annotations and signatures of Neovim apis
+            "folke/lazydev.nvim",
+            dependencies = {
+
+                { "Bilal2453/luvit-meta", lazy = true },
+            },
+            ft = "lua",
+            opts = {
+                library = {
+                    -- Load luvit types when the `vim.uv` word is found
+                    { path = "luvit-meta/library", words = { "vim%.uv" } },
+                },
+            },
+        },
 
         {
             "folke/trouble.nvim",

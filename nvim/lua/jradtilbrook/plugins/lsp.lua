68fe02704c25a84b5be3a4d41a0e85d503b8fe7c
jradtilbrook <jarryd@tilbrooktech.com>
2024-10-05 16:18:53 +0800

    Update conform formatting config

--- nvim/lua/jradtilbrook/plugins/lsp.lua
+++ nvim/lua/jradtilbrook/plugins/lsp.lua
@@ -7,7 +7,6 @@ return {
         "williamboman/mason-lspconfig.nvim",
         -- Handy schemas for json and yaml
         "b0o/schemastore.nvim",
-        -- Inject extra things into LSP
         {
             "stevearc/conform.nvim",
             event = { "BufWritePre" },
@@ -16,40 +15,37 @@ return {
                 {
                     "<leader>cf",
                     function()
-                        require("conform").format({ async = true, lsp_format = "fallback" })
+                        require("conform").format({ async = true })
                     end,
                     mode = "",
                     desc = "[F]ormat buffer",
                 },
             },
             opts = {
-                notify_on_error = false,
-                format_on_save = function(bufnr)
-                    -- Disable "format_on_save lsp_fallback" for languages that don't
-                    -- have a well standardized coding style. You can add additional
-                    -- languages here or re-enable it for the disabled ones.
-                    local disable_filetypes = { c = true, cpp = true }
-                    local lsp_format_opt
-                    if disable_filetypes[vim.bo[bufnr].filetype] then
-                        lsp_format_opt = "never"
-                    else
-                        lsp_format_opt = "fallback"
-                    end
-                    return {
-                        timeout_ms = 5000,
-                        lsp_format = lsp_format_opt,
-                    }
-                end,
+                format_on_save = { async = true },
+                default_format_opts = {
+                    async = true,
+                    lsp_format = "fallback",
+                },
                 formatters_by_ft = {
+                    go = { "goimports", "gofmt", "golangci_lint" },
+                    javascript = { "eslint", "prettier" },
                     lua = { "stylua" },
-                    php = { "php_cs_fixer" },
-                    -- Conform can also run multiple formatters sequentially
-                    -- python = { "isort", "black" },
-                    --
-                    -- You can use 'stop_after_first' to run the first available formatter from the list
-                    -- javascript = { "prettierd", "prettier", stop_after_first = true },
+                    php = function(bufnr)
+                        local bufname = vim.api.nvim_buf_get_name(bufnr)
+                        -- use php-cs-fixer for HE related repos
+                        if bufname:match("/he/") then
+                            return { "php_cs_fixer" }
+                        end
+                        return { "pint" }
+                    end,
+                    terraform = { "terraform_fmt" },
+                    typescriptreact = { "eslint", "prettier" },
                 },
             },
+            init = function()
+                vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+            end,
         },
 
         -- Useful status updates for LSP

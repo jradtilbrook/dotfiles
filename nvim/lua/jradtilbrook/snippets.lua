90c8ce0f8354f4a48e9acc2a76841225f81878c3
jradtilbrook <jarryd@tilbrooktech.com>
2024-10-05 17:41:29 +0800

    Add snippet for $this

--- nvim/lua/jradtilbrook/snippets.lua
+++ nvim/lua/jradtilbrook/snippets.lua
@@ -4,6 +4,7 @@ local i = ls.insert_node
 local t = ls.text_node
 
 ls.add_snippets("php", {
+    s("t", { t("$this") }),
     s("t.", { t("$this->"), i(0) }),
     s("tink", { t("eval(\\Psy\\sh());") }),
 })

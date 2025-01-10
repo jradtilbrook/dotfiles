a0b06648fc47fee59f5204448002023f0e28833f
jradtilbrook <jarryd@tilbrooktech.com>
2025-01-10 15:07:24 +0800

    Allow disabling auto format on save and extending timeout for eslint

--- nvim/lua/jradtilbrook/plugins/conform.lua
+++ nvim/lua/jradtilbrook/plugins/conform.lua
@@ -1,39 +1,58 @@
 return {
     "stevearc/conform.nvim",
-    event = { "BufWritePre" },
+    event = { "VeryLazy" },
     cmd = { "ConformInfo" },
     keys = {
         {
             "<leader>cf",
             function()
-                require("conform").format({ async = true })
+                require("conform").format({ timeout_ms = 10000 })
             end,
             mode = "",
             desc = "[F]ormat buffer",
         },
     },
-    opts = {
-        format_on_save = { timeout_ms = 5000 },
-        default_format_opts = {
-            async = true,
-            lsp_format = "fallback",
-        },
-        formatters_by_ft = {
-            go = { "goimports", "gofmt", "golangci_lint" },
-            javascript = { "eslint", "prettier" },
-            lua = { "stylua" },
-            php = function(bufnr)
-                local bufname = vim.api.nvim_buf_get_name(bufnr)
-                -- use php-cs-fixer for HE related repos
-                if bufname:match("/he/") then
-                    return { "php_cs_fixer" }
+    config = function()
+        local enable_format_on_save = true
+
+        vim.keymap.set("", "<leader>cT", function()
+            enable_format_on_save = not enable_format_on_save
+        end, { desc = "Toggle auto [F]ormatting" })
+
+        require("conform").setup({
+            format_on_save = function(bufnr)
+                if enable_format_on_save then
+                    -- if we are running eslint, have a huge timeout
+                    local formatters = require("conform").list_formatters_to_run(bufnr)
+                    for _, v in pairs(formatters) do
+                        if v.name == "eslint_d" then
+                            return { timeout_ms = 10000 }
+                        end
+                    end
+                    return {}
                 end
-                return { "pint" }
             end,
-            terraform = { "terraform_fmt" },
-            typescriptreact = { "eslint", "prettier" },
-        },
-    },
+            default_format_opts = {
+                lsp_format = "fallback",
+                timeout_ms = 2000,
+            },
+            formatters_by_ft = {
+                go = { "goimports", "gofmt", "golangci_lint" },
+                javascript = { "eslint_d", "prettierd" },
+                typescript = { "eslint_d", "prettierd" },
+                lua = { "stylua" },
+                php = function(bufnr)
+                    local bufname = vim.api.nvim_buf_get_name(bufnr)
+                    if bufname:match("/he/") then
+                        return { "php_cs_fixer" }
+                    end
+                    return { "pint" }
+                end,
+                terraform = { "terraform_fmt" },
+                typescriptreact = { "eslint_d", "prettierd" },
+            },
+        })
+    end,
     init = function()
         vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
     end,

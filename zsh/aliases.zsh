25fb662bad6e4d08964727ee2be3a832c6ccf5ee
jradtilbrook <jarryd@tilbrooktech.com>
2025-01-16 09:25:06 +0800

    Update gum

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,7 +2,7 @@ alias v=nvim
 alias vimdiff='nvim -d'
 alias t=tmux
 alias cat=bat
-alias sc='sesh connect "$(sesh list -i | gum filter --limit 1 --placeholder "Pick a sesh" --prompt="⚡")"'
+alias sc='sesh connect "$(sesh list -i | gum filter --no-strip-ansi --limit 1 --placeholder "Pick a sesh" --prompt="⚡")"'
 alias tree='eza --tree --color=always'
 alias kx='kubectx'
 

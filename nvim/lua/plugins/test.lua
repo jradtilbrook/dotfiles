7160958a3096f1a5d66cabed9625cdcbb11c76bc
jradtilbrook <jarryd@tilbrooktech.com>
2025-08-22 14:21:29 +0800

    Some fixes for neotest

--- nvim/lua/plugins/test.lua
+++ nvim/lua/plugins/test.lua
@@ -1,11 +1,21 @@
 return {
   { "marilari88/neotest-vitest", lazy = true },
   { "nvim-neotest/neotest-jest", lazy = true },
+  { "jradtilbrook/neotest-pest", branch = "ts-fix", lazy = true }, -- using my fork to fix treesitter query
+  -- { "V13Axel/neotest-pest", enabled = false },
+  { "olimorris/neotest-phpunit", lazy = true },
   {
     "nvim-neotest/neotest",
-    opts = function(_, opts)
-      table.insert(opts.adapters, "neotest-vitest")
-      table.insert(opts.adapters, "neotest-jest")
-    end,
+    commit = "52fca6717ef972113ddd6ca223e30ad0abb2800c", -- TEMP: bug on latest commit
+    opts = {
+      adapters = {
+        "neotest-vitest",
+        "neotest-jest",
+        "neotest-pest",
+        ["neotest-phpunit"] = {
+          root_ignore_files = { "tests/Pest.php" },
+        },
+      },
+    },
   },
 }

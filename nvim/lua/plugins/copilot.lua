2c3c7f8d673419fbe4142b668e6a8f907874b775
jradtilbrook <jarryd@tilbrooktech.com>
2025-08-27 21:51:22 +0800

    Set explicit node for copilot

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -3,4 +3,11 @@ return {
   opts = {
     auto_insert_mode = false,
   },
+  {
+    "zbirenbaum/copilot.lua",
+    lazy = true,
+    opts = {
+      copilot_node_command = vim.fn.expand("$NVM_DIR") .. "/versions/node/v22.18.0/bin/node",
+    },
+  },
 }

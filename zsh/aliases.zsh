4be7b224d2ea1ddc3b3deb7a0f74ca46d1ffbb4c
jradtilbrook <jarryd@tilbrooktech.com>
2026-01-16 15:34:09 +0800

    Lazygit alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,4 @@ alias cat=bat
 alias sc='sesh connect "$(sesh list -i --hide-duplicates --hide-attached | gum filter --no-strip-ansi --limit 1 --placeholder "Pick a sesh" --prompt="⚡")"'
 alias tree='eza --tree --color=always'
 alias kx='kubectx'
+alias lg='lazygit'
